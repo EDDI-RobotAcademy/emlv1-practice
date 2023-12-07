fd7b9a1ac0ba56eb5aa32df53fbae9b296732bb8
PSH\psh30 <ske1222@naver.com>
2023-12-07 13:55:55 +0900

    feat : [Sang Ho Park] 특정 기능이 Console로 입출력을 요청할 때 해당 요청을 처리할 입출력 함수를 작성합니다. [EMVIP-UI7]

--- ui/console/service/console_service_call_table.h
+++ ui/console/service/console_service_call_table.h
@@ -1,7 +1,19 @@
 #ifndef __CONSOLE_SERVICE_CALL_TABLE_H__
 #define __CONSOLE_SERVICE_CALL_TABLE_H__
 
+#include <stdio.h>
+#include "console_service.h"
 
+typedef void (* member_call_ptr_t) (void);
 
+void console_protocol_not_implemented (void)
+{
+    printf("아직 구현되지 않은 스펙입니다!\n");
+}
+
+const console_service_call_ptr_t cosnole_service_call_table[CONSOLE_SERVICE_CALL_BUFFER_COUNT] = {
+        [0 ... CONSOLE_SERVICE_CALL_BUFFER] = &console_protocol_not_implemented,
+#include "console_service_call_table_mapper.h"
+};
 
 #endif
--- ui/console/service/console_service_handler.c
+++ ui/console/service/console_service_handler.c
@@ -1,1 +1,44 @@
 #include "console_service_handler.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+
+inline void console_get_user_input (char *user_input, unsigned int user_input_max_size)
+{
+    read(STDIN, user_input, user_input_size);
+}
+
+inline void console_put_output_msg (char *output_msg, unsigned int output_msg_size)
+{
+    write(STDOUT, output_msg, output_msg_size);
+}
+
+unsigned int console_get_user_input_with_msg (char *output_msg, char *user_input, unsigned int user_input_max_size)
+{
+    console_put_output_msg(output_msg, strlen(output_msg));
+    console_get_user_input(user_input, user_input_size);
+}
+
+unsigned int console_get_hidden_user_input_with_msg(char *output_msg, char *hidden_user_input, unsigned int user_input_max_size)
+{
+    struct termios oFlags, nFlags;
+    int length = 0;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    console_put_output_msg(output_msg, strlen(output_msg));
+    console_get_user_input(user_input, user_input_size);
+
+    length = strlen(hidden_user_input);
+    hidden_user_input[length] = '\0';
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+}
