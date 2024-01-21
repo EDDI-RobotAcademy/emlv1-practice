8801d81b822e5486a82fb199f5e24588ffd37a9e
oqazett <oqazett@gmail.com>
2024-01-21 15:40:32 +0900

    fix : [Gwang Seon Eo] CMakeLists.txt 수정 main.c에 환영메세지 추가

--- main/main.c
+++ main/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main(void){
+#include "../ui/console/service/console_service.h"
 
-    printf("test\n");
+void main_board_controller(void);
+
+int main(void){
+    
+    main_board_controller();
     return 0;
+}
+
+void main_board_controller(void){
+    
+    while(!check_user_input_close())
+    {
+        print_welcome_msg();
+        print_menu_msg();
+    }
 }
